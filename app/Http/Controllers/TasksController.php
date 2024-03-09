3758b38311f4a40305ec4329e8b3c7ac9a58d3c6
daichi.sato <mera2_7@yahoo.co.jp>
2024-03-09 14:05:02 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -50,11 +50,13 @@ class TasksController extends Controller
         
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',
            'content' => 'required|max:255',
         ]);
         
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -108,12 +110,14 @@ class TasksController extends Controller
         
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',
            'content' => 'required|max:255',
         ]);
         
                // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->statys = $request->status;
         $task->content = $request->content;
         $task->save();
 
