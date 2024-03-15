4c77e1231ae3f5cd857d0fb6ce9d370cf8b93c2e
daichi.sato <mera2_7@yahoo.co.jp>
2024-03-15 13:53:44 +0000

    top page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="prose mx-auto text-center">
+        <h2>Log in</h2>
+    </div>
+
+    <div class="flex justify-center">
+        <form method="POST" action="{{ route('login') }}" class="w-1/2">
+
+
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
